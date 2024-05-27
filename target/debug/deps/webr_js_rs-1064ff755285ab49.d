d3524213a4888d7af8f0b9e6a1e6e6b1b1bbae66
Josiah Parry <josiah.parry@gmail.com>
2024-05-27 13:02:51 -0400

    remove target

--- target/debug/deps/webr_js_rs-1064ff755285ab49.d
+++
@@ -1,5 +0,0 @@
-/Users/josiahparry/github/rust/webr-js-rs/target/debug/deps/libwebr_js_rs-1064ff755285ab49.rmeta: src/lib.rs
-
-/Users/josiahparry/github/rust/webr-js-rs/target/debug/deps/webr_js_rs-1064ff755285ab49.d: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/webr_js_rs-551ade767987c78f.d
+++
@@ -1,5 +0,0 @@
-/Users/josiahparry/github/rust/webr-js-rs/target/debug/deps/libwebr_js_rs-551ade767987c78f.rmeta: src/lib.rs
-
-/Users/josiahparry/github/rust/webr-js-rs/target/debug/deps/webr_js_rs-551ade767987c78f.d: src/lib.rs
-
-src/lib.rs:
